9f4dd12cfe1d9f7602da284cf819ec56a8c32e8b
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2024-06-06 09:14:19 +0700

    Publish

---
+++ src/main/java/dai_ichi_customer_portal/shared_modules/SharedModulesApplication.java
@@ -0,0 +1,13 @@
+package dai_ichi_customer_portal.shared_modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedModulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dai_ichi_customer_portal/shared_modules/SharedModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package dai_ichi_customer_portal.shared_modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
