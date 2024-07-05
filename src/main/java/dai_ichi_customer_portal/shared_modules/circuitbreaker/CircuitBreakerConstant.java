682c5b45f7f45256dde84a6da8deaa21eeeed070
Kong Bunthoeurn <bunthoeurn.kong@igsaas.asia>
2024-07-05 11:51:44 +0700

    add cb service

---
+++ src/main/java/dai_ichi_customer_portal/shared_modules/circuitbreaker/CircuitBreakerConstant.java
@@ -0,0 +1,8 @@
+package dai_ichi_customer_portal.shared_modules.circuitbreaker;
+
+public class CircuitBreakerConstant {
+    public static String EXTERNAL_CUSTOMER_PORTAL_SERVICE = "externalCustomerPortalService";
+    public static String EXTERNAL_AUTH_SERVICE = "externalAuthService";
+    public static String AUTH_SERVICE = "authService";
+    public static String CUSTOMER_PORTAL_SERVICE = "customerPortalService";
+}
