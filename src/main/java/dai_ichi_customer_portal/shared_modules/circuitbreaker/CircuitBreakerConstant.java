ebebc3a2dba5c40ba38f7b01b98c5e979172ee2e
Kong Bunthoeurn <bunthoeurn.kong@igsaas.asia>
2024-07-11 17:54:17 +0700

    add constant

--- src/main/java/dai_ichi_customer_portal/shared_modules/circuitbreaker/CircuitBreakerConstant.java
+++ src/main/java/dai_ichi_customer_portal/shared_modules/circuitbreaker/CircuitBreakerConstant.java
@@ -3,6 +3,7 @@ package dai_ichi_customer_portal.shared_modules.circuitbreaker;
 public class CircuitBreakerConstant {
     public static final String EXTERNAL_CUSTOMER_PORTAL_SERVICE = "externalCustomerPortalService";
     public static final String EXTERNAL_AUTH_SERVICE = "externalAuthService";
+    public static final String EXTERNAL_SMS_SERVICE = "externalSMSService";
     public static final String AUTH_SERVICE = "authService";
     public static final String CUSTOMER_PORTAL_SERVICE = "customerPortalService";
 }
